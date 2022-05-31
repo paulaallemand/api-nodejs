339afc3b2242e7443e7f40879d367ef8f5b40949
Paula Allemand <paulaalemand@gmail.com>
2022-05-31 11:43:58 -0300

    first commit full project :eyes:

--- src/app.js
+++ src/app.js
@@ -7,5 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 // importar as rotas
+const movies = require("./routes/moviesRoutes");
+
+// definir rota padrão
+app.use("/movies", movies);
 
 module.exports = app;
